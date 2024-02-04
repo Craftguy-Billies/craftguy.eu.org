658f0e23507df5f8a45d7c3fedda184b52843614
Craftguy-Billies <69667779+Craftguy-Billies@users.noreply.github.com>
2024-02-05 00:29:22 +0800

    Create algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,28 @@
+function count_consistency(){
+    var arr = []
+    for(var i=0;i<5;i++){
+        var points = document.getElementsByClassName("points")[i].value;
+        if(points.trim() !== "" && typeof Number(points.trim()) === "number"){
+            arr.push(Number(points))
+        }
+    }
+
+    var c_array = arr.reverse();
+    var sum = 0;
+
+    for (var i = 0; i < c_array.length; i++) {
+        var weighted_sum = 0;
+        sum += sum_of_reciprocals(weighted_sum, c_array.length, i, i);
+    }
+
+    function sum_of_reciprocals(w_sum, days, current, index) {
+        if (index < days) {
+            w_sum += c_array[current] / (index + 1);
+            return sum_of_reciprocals(w_sum, days, current, index + 1);
+        } else {
+            return w_sum / days;
+        }
+    }
+
+    document.getElementById("consistency").innerHTML = sum;
+}
